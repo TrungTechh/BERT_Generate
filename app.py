47341097b94508fc0098fe19e9b74c362ebad348
TrungTechh <deptraihah123@gmail.com>
2023-05-16 22:46:49 +0700

    style website

--- app.py
+++ app.py
@@ -2,23 +2,23 @@ from flask import Flask, request
 from flask_cors import CORS
 import numpy as np
 import torch
-from pytorch_transformers import BertTokenizer, BertForMaskedLM
+from pytorch_transformers import DistilBertTokenizer, DistilBertForMaskedLM
 import nltk
 
 app = Flask(__name__)
 CORS(app)
 
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = BertForMaskedLM.from_pretrained('bert-base-uncased', output_attentions=True)
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+model = DistilBertForMaskedLM.from_pretrained('distilbert-base-uncased', output_attentions=True)
 model.eval()
 
 @app.route('/fillblanks', methods=['POST'])
 def predict():
 	sentence_orig = request.form.get('text')
-	if '____' not in sentence_orig:
+	if '_' not in sentence_orig:
 		return sentence_orig
 
-	sentence = sentence_orig.replace('____', 'MASK')
+	sentence = sentence_orig.replace('_', 'MASK')
 	tokens = nltk.word_tokenize(sentence)
 	sentences = nltk.sent_tokenize(sentence)
 	sentence = " [SEP] ".join(sentences)
@@ -38,7 +38,7 @@ def predict():
 	segments_tensors = torch.tensor([segments_ids])
 
 	with torch.no_grad():
-	    outputs = model(tokens_tensor, token_type_ids=segments_tensors) 
+	    outputs = model(tokens_tensor) 
 	    predictions = outputs[0] 
 	    attention = outputs[-1] 
 
@@ -58,8 +58,8 @@ def predict():
 	predicted_index = torch.argmax(predictions[0, masked_index]).item()
 	predicted_token = tokenizer.convert_ids_to_tokens([predicted_index])[0]
 	for f in focus:
-		sentence_orig = sentence_orig.replace(f, '<font color="blue">'+f+'</font>')
-	return sentence_orig.replace('____', '<font color="red"><b><i>'+predicted_token+'</i></b></font>')
+		sentence_orig = sentence_orig.replace(f, '<font color="#fff">'+f+'</font>')
+	return sentence_orig.replace('_', '<font id ="out" color="red"><b><i>'+predicted_token+'</i></b></font>')
 
 if __name__=='__main__':
 	app.run(host="0.0.0.0", port=7860)
